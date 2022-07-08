37a51c5077177c744300b9a536af0794eaa83d79
liguanghao <liguanghao@viatomtech.com>
2022-07-08 17:17:39 +0800

    sdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 
-import 'DropdownListDemo.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(Example());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Example extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ExampleState();
+}
 
-// This widget is the root of your application.
+class _ExampleState extends State<Example> {
+//  List<String> _locations = ['Please choose a location', 'A', 'B', 'C', 'D']; // Option 1
+//  String _selectedLocation = 'Please choose a location'; // Option 1
+  List<String> _locations = ['A', 'B', 'C', 'D']; // Option 2
+   String _selectedLocation='A'; // Option 2
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          brightness: Brightness.light
+      home: Scaffold(
+        body: Center(
+          child: DropdownButton(
+            hint: Text('Please choose a location'), // Not necessary for Option 1
+            value: _selectedLocation,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedLocation = newValue.toString();
+              });
+            },
+            items: _locations.map((location) {
+              return DropdownMenuItem(
+                child: new Text(location),
+                value: location,
+              );
+            }).toList(),
+          ),
+        ),
       ),
-
-      darkTheme: ThemeData(
-          brightness: Brightness.dark
-      ),
-
-      themeMode: ThemeMode.dark,
-      home: const DropdownListDemo(),
     );
   }
 }
